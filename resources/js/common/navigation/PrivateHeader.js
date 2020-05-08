d225b8340d3ffb5b380859c0a76b340b75f4f959
galindo.ggll <galindo.ggll@sprobe.com>
2020-05-08 12:39:11 +0800

    Fix registration for player

--- resources/js/common/navigation/PrivateHeader.js
+++ resources/js/common/navigation/PrivateHeader.js
@@ -18,71 +18,42 @@ class PrivateHeader extends Component {
     super(props);
   }
 
-  renderNavItems() {
-    const {user} = this.props
-    if (user.type === 0) {
-      return (
-        <nav className="my-2 my-md-0 mr-md-3">
-          <a className="p-2 text-dark" href="/">Home</a>
-          <a className="p-2 text-dark" href="/agents">Agents</a>
-          <a className="p-2 text-dark" href="/players">Players</a>
-          <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-        </nav>)
-    }
-    if (user.type === 1) {
-      return (
-        <nav className="my-2 my-md-0 mr-md-3">
-          <a className="p-2 text-dark" href="/">Home</a>
-          <a className="p-2 text-dark" href={`/players/agent-list/${user.id}`}>Players</a>
-          <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-        </nav>)
-    }
-    if (user.type === 2) {
-      return (
-        <nav className="my-2 my-md-0 mr-md-3">
-          <a className="p-2 text-dark" href="/">Home</a>
-          <a className="p-2 text-dark" href={`/players/view-report/${user.id}/${user.type}`}>Report</a>
-          <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-        </nav>)
-    }
-  }
-
   render() {
     const {user} = this.props
-    return (
-      <div
-        className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 className="my-0 mr-md-auto font-weight-normal">Bluebird Gaming Inc.</h5>
-        <nav className="my-2 my-md-0 mr-md-3">
-          {
-            user.type === 0 &&
-            <nav className="my-2 my-md-0 mr-md-3">
-              <a className="p-2 text-dark" href="/">Home</a>
-              <a className="p-2 text-dark" href="/agents">Agents</a>
-              <a className="p-2 text-dark" href="/players">Players</a>
-              <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-            </nav>
-          }
-          {
-            user.type === 1 &&
-            <nav className="my-2 my-md-0 mr-md-3">
-              <a className="p-2 text-dark" href="/">Home</a>
-              <a className="p-2 text-dark" href={`/players/agent-list/${user.id}`}>Players</a>
-              <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-            </nav>
-          }
-          {
-            user.type === 2 &&
-            <nav className="my-2 my-md-0 mr-md-3">
-              <a className="p-2 text-dark" href="/">Home</a>
-              <a className="p-2 text-dark" href={`/players/view-report/${user.id}/${user.type}`}>Report</a>
-              <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
-            </nav>
-          }
-        </nav>
-        <a className="btn btn-outline-danger" onClick={e => this.props.logout(e)}>Log Out</a>
-      </div>
-    );
+    if (user) {
+      return (
+        <div
+          className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+          <h5 className="my-0 mr-md-auto font-weight-normal">Bluebird Gaming Inc.</h5>
+            {
+              user.type === 0 &&
+              <nav className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-dark" href="/">Home</a>
+                <a className="p-2 text-dark" href="/agents">Agents</a>
+                <a className="p-2 text-dark" href="/players">Players</a>
+                <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
+              </nav>
+            }
+            {
+              user.type === 1 &&
+              <nav className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-dark" href="/">Home</a>
+                <a className="p-2 text-dark" href={`/players/agent-list/${user.id}`}>Players</a>
+                <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
+              </nav>
+            }
+            {
+              user.type === 2 &&
+              <nav className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-dark" href="/">Home</a>
+                <a className="p-2 text-dark" href={`/players/view-report/${user.id}/${user.type}`}>Report</a>
+                <a className="p-2 text-dark" href={`profile/${user.id}`}>Profile</a>
+              </nav>
+            }
+          <a className="btn btn-outline-danger" onClick={e => this.props.logout(e)}>Log Out</a>
+        </div>
+      );
+    }
   }
 }
 
