f1fa0962e45b167e07083576a4e8ab4b64bf64d5
galindo.ggll <galindo.ggll@sprobe.com>
2020-05-14 10:57:43 +0800

    Add loading in transition from public to private and vice versa

--- resources/js/routes/Private.js
+++ resources/js/routes/Private.js
@@ -2,10 +2,25 @@ import React, {Suspense} from 'react'
 import PropTypes from 'prop-types'
 import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Loader from 'react-loader-spinner'
 
 const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
     return <Route {...rest} render={props => {
-        return <Suspense fallback={<div>Loading...</div>}>
+        return <Suspense fallback={
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-auto">
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={100}
+                  width={100}
+                  timeout={2000} //3 secs
+                />
+              </div>
+            </div>
+          </div>
+        }>
             {
                 isAuthenticated
                     ? <Component {...props}/>
--- resources/js/routes/Public.js
+++ resources/js/routes/Public.js
@@ -1,10 +1,23 @@
 import React, { Suspense } from 'react'
 import PropTypes from 'prop-types'
 import { Route } from 'react-router-dom'
+import Loader from 'react-loader-spinner'
 
 const PublicRoutes = ({ component: Component, ...rest }) => {
   return <Route {...rest} render={props => {
-    return <Suspense fallback={<div>Loading...</div>}>
+    return <Suspense fallback={<div className="container">
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={2000} //3 secs
+          />
+        </div>
+      </div>
+    </div>}>
       <Component {...props}/>
     </Suspense>
   }}/>
